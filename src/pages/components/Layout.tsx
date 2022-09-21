07a4445f76cdcebca60fc3cf217fc107a951efad
Anurag <anuragsharma011011@gmail.com>
2022-09-21 14:32:18 +0530

    remove lang

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,14 +1,12 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Html } from "next/document";
+// import { Html } from "next/document";
 
 export default function Layout({ children }: { children: any }) {
   return (
     <>
-      <Html lang='en'>
       <Navbar />
       {children}
       <Footer/>
-      </Html>
     </>
   )};
