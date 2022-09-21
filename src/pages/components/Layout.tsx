97d33bfd3cfd09bfb2f998a6ee3e07f03fad25ae
Anurag <anuragsharma011011@gmail.com>
2022-09-21 14:25:49 +0530

    seo or fast

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Html } from "next/document";
 
 export default function Layout({ children }: { children: any }) {
   return (
     <>
+      <Html lang='en'>
       <Navbar />
       {children}
       <Footer/>
+      </Html>
     </>
   )};
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -7,6 +7,8 @@ const Navbar: NextPage = () => {
     <>
       <Head>
         <title>anurag</title>
+        <meta name="description" content="Hey There I am Anurag , I build things"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       <Script src="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,300,400&display=swap" />
       <Script src="https://api.fontshare.com/v2/css?f[]=chillax@600,500,300,400&display=swap" />
