fa90f6b2f48421ffa6a581cce998b63f0ab2b64e
Anurag <anuragsharma011011@gmail.com>
2022-09-26 18:38:57 +0530

    ??

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-// import { Html } from "next/document";
 
 export default function Layout({ children }: { children: any }) {
   return (
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -10,8 +10,6 @@ const Navbar: NextPage = () => {
         <meta name="description" content="Hey There I am Anurag , I build things"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
-      <Script src="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,300,400&display=swap" />
-      <Script src="https://api.fontshare.com/v2/css?f[]=chillax@600,500,300,400&display=swap" />
     </>
   );
 };
