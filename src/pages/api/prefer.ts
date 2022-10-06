663d1a15a724e563692880d67b30cbdf3d2027ad
Anurag <anuragsharma011011@gmail.com>
2022-10-07 00:24:20 +0530

    prefers

---
+++ src/pages/api/prefer.ts
@@ -0,0 +1,4 @@
+import { NextApiRequest ,NextApiResponse  } from "next";
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).json({ programs: ['vscode', 'vscode insiders','git','npm & yarn','windows terminal','postman','planetscale','mongodb compass','brave','vivalid browser','Ubuntu WSL','vlc'] , vscode:['Github copilot','Prettier','Karma','Material Icon theme','Prisma','WakaTime','coderunner','auto close & rename & import tag','ES7+ React' ,'Javascript/ts'],browser:['awesome ss','metamask','unhook yt','sponserblock yt','audio only yt','grammerly','emoji keyboard'] });
+}
